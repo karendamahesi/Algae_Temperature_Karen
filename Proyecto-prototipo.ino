fde886037aa407c1c29ad8662658938137e33d1f
karendamahesi <cruzpavon.karen@gmail.com>
2024-05-07 21:22:07 -0600

    Estados

--- Proyecto-prototipo.ino
+++ Proyecto-prototipo.ino
@@ -1,31 +1,47 @@
 // Karen Cruz 2024
 
-//Incluir la libreria de LiquidCrystal
+//Include LiquidCrystal libreries
 #include <LiquidCrystal.h>
 #include <Wire.h>
 
 //Crear el objeto lcd  dirección  32 (depende del modelo) y 16 columnas x 2 filas
 LiquidCrystal lcd(32,16,2);  //
 
-void setup() {
-  // put your setup code here, to run once:
-//Para iniciar la libreria y comunicación I2C con Arduino
-  	Wire.begin(); 
-  
-	// Inicializar el LCD
-	lcd.init();
-  
-	//Encender la luz de fondo.
-	lcd.backlight();
+// Pin definition for LCD screen
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-  // Tipo de dato y variable
-  float Termistor; 
+// Definition of pins and variables for the temperature sensor and the water pump
+const int sensorPin = A0; // Analog pin for temperature sensor
+const int bombaPin = 9;    // Digital pin for water pump
+int Temperatura;           // Any data type for measured temperature variable
+
+// Definition of the states
+enum Estado { 
+  SENSOR_ACTIVADO
+  ANALISIS_TEMPERATURA
+  REMAPEO_VALORES
+  ACTIVACION
+  DESACTIVACION
+  ENVIO_DATOS_NUBE}; 
+Estado estado=SENSOR_ACTIVADO; //Initial state
+
+
+void setup() {
+	// LCD screen initialization
+	lcd.begin(16, 2);
+  
+	// Water pump pin initialization
+  pinMode(bombaPin, OUTPUT);
   }
 
+
+void readSensor() {
+  Serial.printIn ("Reading sensor");
+  Termistor=map(Temperatura, 0, 1023, -80, 150); //nuevo mapeo de temperatura
+}
+
 void loop() {
-  // put your main code here, to run repeatedly:
-// Nuevo mapeo de temperatura
-Termistor=map(Temperatura, 0, 1023, -80, 150);
 
 lcd.clear();
 lcd.setCursor (0, 0);
