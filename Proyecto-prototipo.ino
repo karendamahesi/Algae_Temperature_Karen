10ce5d6ca32728e9a450f080d13db1420df33703
karendamahesi <cruzpavon.karen@gmail.com>
2024-05-28 16:00:51 -0600

    Errores Arduino

--- Proyecto-prototipo.ino
+++ Proyecto-prototipo.ino
@@ -23,7 +23,7 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 const int sensorPin = A0; // Analog pin for temperature sensor
 const int bombaPin = 9;    // Digital pin for water pump
 const int relePin = 8;     // Digital pin to control the relay
-int Temperatura;           // Any data type for measured temperature variable
+float temperature;           // Any data type for measured temperature variable
 const int chipSelect = 10; // Pin para el chip select de la tarjeta SD
 
 
@@ -40,8 +40,6 @@ enum Estado {
   SEND_CLOUD,
 }
 
-Estado estado=READ_SENSOR; //Initial state
-
 
 // Variables para Arduino IoT Cloud
 float temperature;
@@ -49,18 +47,29 @@ String currentTime;
 
 
 // Definición de tu red WiFi y credenciales de Arduino IoT Cloud
-const char SSID[]     = COQUIS;    // Tu SSID
-const char PASS[]     = espejocarretillaprincesa; // Tu contraseña de red
-const char DEVICE_ID[]  = 630d9091-cb70-48b1-9f55-27ff4ab9f2ff;  // Tu Device ID de Arduino IoT Cloud
+const char SSID[]     = "COQUIS";    // Tu SSID
+const char PASS[]     = "espejocarretillaprincesa"; // Tu contraseña de red
+const char DEVICE_ID[]  = "630d9091-cb70-48b1-9f55-27ff4ab9f2ff";  // Tu Device ID de Arduino IoT Cloud
 const char DEVICE_SECRET[] = "your_DEVICE_SECRET"; // Tu Device Secret de Arduino IoT Cloud
 
 
-// Inicialización de la conexión WiFi y Arduino IoT Cloud
-  ArduinoCloud.begin(ArduinoIoTPreferredConnection);
-  ArduinoCloud.addProperty(temperature, READWRITE, ON_CHANGE, NULL);
-  ArduinoCloud.addProperty(currentTime, READWRITE, ON_CHANGE, NULL);
-  setDebugMessageLevel(2);
-  ArduinoCloud.printDebugInfo();
+// NTP Client para obtener la hora de internet
+WiFiUDP ntpUDP;
+const long utcOffsetInSeconds = -6 * 3600; // Offset de UTC para Ciudad de México (UTC-6)
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds, 60000); // Actualiza cada 60 segundos
+
+
+unsigned long tiempoActual = 0;
+unsigned long tiempoUltimaLectura = 0; // Variable para registrar el tiempo de la última lectura del sensor
+const unsigned long intervaloLectura = 30 * 60 * 1000; // Intervalo de lectura: 30 minutos en milisegundos
+unsigned long tiempoInicioIdle = 0; // Variable para registrar el inicio del estado IDLE
+const unsigned long duracionIdle = 30 * 60 * 1000; // Duración del estado IDLE: 30 minutos en milisegundos
+
+
+
+void setup() {
+   // LCD screen initialization
+	lcd.begin(16, 2);
 
 
   // Conectar a WiFi
@@ -71,38 +80,30 @@ const char DEVICE_SECRET[] = "your_DEVICE_SECRET"; // Tu Device Secret de Arduin
   } 
 
 
-// NTP Client para obtener la hora de internet
-WiFiUDP ntpUDP;
-const long utcOffsetInSeconds = -6 * 3600; // Offset de UTC para Ciudad de México (UTC-6)
-NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds, 60000); // Actualiza cada 60 segundos
+  //Inicialización de la variable estado
+  Estado estado=READ_SENSOR; //Estado inicial
 
 
-unsigned long tiempoUltimaLectura = 0; // Variable para registrar el tiempo de la última lectura del sensor
-const unsigned long intervaloLectura = 30 * 60 * 1000; // Intervalo de lectura: 30 minutos en milisegundos
-unsigned long tiempoInicioIdle = 0; // Variable para registrar el inicio del estado IDLE
-const unsigned long duracionIdle = 30 * 60 * 1000; // Duración del estado IDLE: 30 minutos en milisegundos
+  // Iniciar la cola de tareas
+  queueIndex = 0;
+  Estado = READ_SENSOR;
 
 
-// Inicialización del cliente NTP
-  timeClient.begin();
+// Inicialización de la conexión WiFi y Arduino IoT Cloud
+  ArduinoIoTCloud.begin(ArduinoIoTPreferredConnection);
+  ArduinoIoTCloud.addProperty(temperature, READWRITE, ON_CHANGE, NULL);
+  ArduinoIoTCloud.addProperty(currentTime, READWRITE, ON_CHANGE, NULL);
+  setDebugMessageLevel(2);
+  ArduinoIoTCloud.printDebugInfo();
 
-
-void setup() {
-	// LCD screen initialization
-	lcd.begin(16, 2);
-  
 
 	// Water pump and relay pin initialization
   pinMode(bombaPin, OUTPUT);
   pinMode(relePin, OUTPUT);
   
- 
-// Inicialización de la conexión WiFi y Arduino IoT Cloud
-  ArduinoCloud.begin(ArduinoIoTPreferredConnection);
-  ArduinoCloud.addProperty(temperature, READWRITE, ON_CHANGE, NULL);
-  ArduinoCloud.addProperty(currentTime, READWRITE, ON_CHANGE, NULL);
-  setDebugMessageLevel(2);
-  ArduinoCloud.printDebugInfo();
+
+  // Inicialización del cliente NTP
+  timeClient.begin();
 
 
   // Inicialización de la tarjeta SD
@@ -121,7 +122,7 @@ void loop() {
   int queueIndex = 0;
 
   // Call the function corresponding to the current state
-  switch (estado) {
+  switch (Estado) {
     case IDLE:
       idle();
       break;
@@ -153,7 +154,7 @@ void loop() {
 
 // Iniciar la cola de tareas
   queueIndex = 0;
-  estado = READ_SENSOR;
+  Estado = READ_SENSOR;
 
 // Procesar la cola de tareas
   processQueue();
@@ -166,11 +167,11 @@ void loop() {
 void processQueue() {
   switch (queue[queueIndex]) {
     case GET_INTERNET_TIME:
-      getInternetTime();
+      get_internet_time();
       queueIndex++;
       break;
     case READ_SENSOR:
-      readSensor();
+      read_sensor();
       queueIndex++;
       break;
   }
@@ -179,7 +180,7 @@ void processQueue() {
 
 void directing() {
     if (tiempoActual % 30000 == 0) {
-    estado = READ_SENSOR;
+    Estado = READ_SENSOR;
   }
   // Obtener la hora actual para verificar si es medianoche
   timeClient.update();
@@ -188,14 +189,14 @@ void directing() {
   int currentSecond = timeClient.getSeconds();
   //Cambiar al estado GET_INTERNET_TIME a media noche
   if (currentHour == 0 && currentMinute == 0 && currentSecond == 0) {
-    estado = GET_INTERNET_TIME; 
+    Estado = GET_INTERNET_TIME; 
   }
 }
 
 
 void get_internet_time(){
   // Obtener el tiempo actual
-  time_t now = now();
+  time_t current_time = now();
   currentTime = String(hour(now)) + ":" + String(minute(now)) + ":" + String(second(now));
 }
 
@@ -225,21 +226,21 @@ void read_sensor() {
   currentTime = timeClient.getFormattedTime();
 
   // Update temperature value in Arduino Cloud
-  ArduinoCloud.updateTemperature(temperatura);
+  ArduinoIoTCloud.updateTemperature(temperatura);
   
   // Switch to next state after a while
   delay(3000); // Wait 3 seconds
-  estado = EVALUAR_TEMP;
+  Estado = EVALUAR_TEMP;
 }
 
 
 void evaluar_temp() {
   // Check if the recorded temperature is >26°C
-  if (temperatura > 26) {
-    estado = ACTIVACION;
+  if (Temperatura > 26) {
+    Estado = ACTIVACION;
   }
   else {
-    estado = DESACTIVACION;
+    Estado = DESACTIVACION;
   }
 }
 
@@ -248,7 +249,7 @@ void activacion() {
   // Turn on the relay that activates the water pump
   digitalWrite(relePin, HIGH);
 
-  estado = IDLE;
+  Estado = IDLE;
 }
 
 
@@ -256,15 +257,15 @@ void desactivacion() {
   // Turn on the relay that activates the water pump
   digitalWrite(relePin, LOW);
 
-  estado = IDLE;
+  Estado = IDLE;
 }
 
 
 void send_cloud(){
   // Update temperature value in Arduino Cloud
-  ArduinoCloud.updateTemperature(temperatura);
+  ArduinoIoTCloud.updateTemperature(Temperatura);
 
-  estado = STORE_SD
+  Estado = STORE_SD
 }
 
 
@@ -276,14 +277,14 @@ void store_sd(){
     dataFile.print("Hora: ");
     dataFile.print(currentTime);
     dataFile.print(", Temperatura: ");
-    dataFile.println(temperature);
+    dataFile.println(Temperature);
     dataFile.close();
     Serial.println("Datos guardados en la tarjeta SD.");
   } else {
     Serial.println("Error abriendo el archivo datalog.txt");
   }
 
-  estado = IDLE
+  Estado = IDLE
 }
 
 
@@ -291,7 +292,7 @@ void idle() {
   // Mantener el estado IDLE durante 30 minutos
   if (millis() - tiempoInicioIdle >= duracionIdle) {
     // Cambiar al siguiente estado después del periodo de inactividad
-    estado = SENSOR_ACTIVADO;
+    Estado = READ_SENSOR;
     tiempoInicioIdle = millis(); // Reiniciar el temporizador de inactividad
   }
 }
